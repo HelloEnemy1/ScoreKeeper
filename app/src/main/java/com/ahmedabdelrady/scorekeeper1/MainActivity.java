19cd2366848aef8bc53e069d3eacc0a58a3a8b22
HelloEnemy1 <abdelradyahmed05@gmail.com>
2025-10-01 14:59:29 -0400

    Initial app from school laptop

---
+++ app/src/main/java/com/ahmedabdelrady/scorekeeper1/MainActivity.java
@@ -0,0 +1,39 @@
+package com.ahmedabdelrady.scorekeeper1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    TextView myTextView;
+    EditText myEditText;
+    Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        myTextView = (TextView) findViewById(R.id.greeting);
+        myEditText = (EditText) findViewById((R.id.typeName));
+        submitButton = (Button) findViewById(R.id.SubmitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = myEditText.getText().toString();
+                myTextView.setText("Hello, " + name + "!");
+
+            }
+
+
+        });
+    }
+}
